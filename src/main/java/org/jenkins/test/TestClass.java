68d479bc81b358aac892a6515da5f24ada0388e4
andrei.urse <andrei.urse1@gmail.com>
2017-01-09 16:54:20 +0100

    Trigger build

--- src/main/java/org/jenkins/test/TestClass.java
+++ src/main/java/org/jenkins/test/TestClass.java
@@ -6,6 +6,6 @@ package org.jenkins.test;
 public class TestClass {
 
     public static void main(String [] args) {
-        System.out.println("test")+error;
+        System.out.println("test");
     }
 }
