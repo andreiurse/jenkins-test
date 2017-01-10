3ae61e136f76dcef6422680e68ddc99e362c9028
andrei.urse <andrei.urse1@gmail.com>
2017-01-10 17:30:46 +0100

    test

--- src/main/java/org/jenkins/test/HelloWorld.java
+++ src/main/java/org/jenkins/test/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
     public static void main(String [] args) {
         System.out.println("test");
-        //test2
+        //test23
     }
 
     public String returnSmth() {
