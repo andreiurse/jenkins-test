bb84c704fe3ab50bf30664d4a817e7eea1d2693b
andrei.urse <andrei.urse1@gmail.com>
2017-01-10 09:37:24 +0100

    Trigger build

--- src/main/java/org/jenkins/test/HelloWorld.java
+++ src/main/java/org/jenkins/test/HelloWorld.java
@@ -3,9 +3,13 @@ package org.jenkins.test;
 /**
  * Created by andrei.urse on 1/9/2017.
  */
-public class TestClass {
+public class HelloWorld {
 
     public static void main(String [] args) {
         System.out.println("test");
+    }
+
+    public String returnSmth() {
+        return "something";
     }
 }
